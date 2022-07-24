7b02b29dfad10b3ef7896476b0c158e792107c43
NipunBinjola <nipun.binjola@procol.in>
2022-07-24 14:25:20 +0530

    file_io init complete

--- system_driver/file_io.rb
+++ system_driver/file_io.rb
@@ -4,11 +4,16 @@ module SystemDriver
   class FileIo
     require './parking_lot_system'
 
-    attr_reader :file_path, :file
+    attr_reader :file_path, :file, :parking_lot_system
 
     def initialize(filepath)
       @file_path = filepath
       @file = File.readlines(filepath).map(&:chomp)
+      init_command = file.first.split
+      raise 'Invalid input file!' unless init_command.first == 'create_parking_lot'
+
+      @parking_lot_system = ParkingLotSystem.new(init_command[1].to_i)
+      puts "Created a parking lot with #{init_command[1]} slots."
     end
 
     def execute; end
